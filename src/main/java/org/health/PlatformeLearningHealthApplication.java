f17f0e7fa12fb2b437ee6cdbe2e6fc9c704d9174
mounir <mounirhamdane@yahoo.fr>
2016-09-16 20:00:41 +0200

    first com

---
+++ src/main/java/org/health/PlatformeLearningHealthApplication.java
@@ -0,0 +1,12 @@
+package org.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformeLearningHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformeLearningHealthApplication.class, args);
+	}
+}
